f85706c61f73edfdb753b681adeada18e09657b9
abhishek <abhishek251119@gmail.com>
2023-06-08 11:44:07 +0530

    if you want to use event in next.js so you will use "user client" in tope of file

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,25 @@
-
+'use client'
 import styles from './page.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+  const [data,setData]=useState('Home Page')
+  const abhi=()=>{
+    setData('i changed Home Page in page with help of useState')
+  }
+  
+  const InnerComp= ()=>{
+   return( 
+    <h1>InnerComponent</h1>
+   )
+
+  }
   return (
     <main className={styles.main}>
-      <User name="Abhishek" />
-     <h1>Home page</h1>
+     <h1> {data}</h1>
+     <button onClick={abhi} onDoubleClick={()=>{setData('home page')}}>click me</button>
+    <InnerComp/>
     </main>
   )
 }
 
-const User = (props)=>{
-  return(
-    <div>
-      <h2>User name is {props.name}</h2>
-    </div>
-  )
-}
